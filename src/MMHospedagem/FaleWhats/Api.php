613b825b68d3fa12005a1444fc924efeb8c528b3
marcosmmhospedagem <marcos.antonio@mmhospedagem.com.br>
2024-01-16 19:11:27 -0300

    Atualização
    att

--- src/MMHospedagem/FaleWhats/Api.php
+++ src/MMHospedagem/FaleWhats/Api.php
@@ -9,6 +9,7 @@ class Api extends FaleWhats {
             "Cache-Control: no-cache",
             "Content-type: application/json"
         ];
+
         $curl = curl_init();
         curl_setopt_array($curl,[
             CURLOPT_URL 			=> 	$endpoint,
@@ -35,12 +36,12 @@ class Api extends FaleWhats {
         $request    =   [
             "numero"   =>  $telefone
         ];
-
         $send = self::send("POST","/{$this->id_sessao}/rest/consultas/{$this->licenca}/numero",$request);
-
-        self::log("[JSON][API][VERIFICAÇÃO DE NÚMERO]: " . json_encode($send));
-
+        if ($this->log){
+            self::log("[JSON][API][VERIFICAÇÃO DE NÚMERO]: " . json_encode($send));
+        }
         return $send;
+
 
     }
 
@@ -48,16 +49,14 @@ class Api extends FaleWhats {
 
         $Numero_Recebedio   =   preg_replace('/\D/', '', $telefone);
         $validacaoNumero    =   self::validar_numero($Numero_Recebedio);
-
         $request    =   [
             "numero"   =>  $telefone,
             "presenca"  =>  $tipo
         ];
-
         $send = self::send("POST","/{$this->id_sessao}/rest/acoes/{$this->licenca}/presenca",$request);
-
-        self::log("[JSON][API][SIMULAR PRESENÇA]: " . json_encode($send));
-
+        if ($this->log){
+            self::log("[JSON][API][SIMULANDO PRESENÇA]: " . json_encode($send));
+        }
         return $send;
 
     }
@@ -80,10 +79,67 @@ class Api extends FaleWhats {
                     self::simular_presenca($validacaoNumero["jid"],'composing');
                 }
                 $send = self::send("POST","/{$this->id_sessao}/rest/envio/{$this->licenca}/texto",$request);
-                self::log("[JSON][API][SEND MSG]: " . json_encode($send));
+                if ($this->log){
+                    self::log("[JSON][API][ENVIO DE MENSAGEM TIPO TEXTO]: " . json_encode($send));
+                }
                 return $send;
             }
         }
     }
 
+    public function log($mensagem, $arquivo = null) {
+
+        $diretorio = dirname(__FILE__) . DIRECTORY_SEPARATOR ."Logs";
+        if (!is_dir($diretorio)) {
+            mkdir($diretorio, 0777, true);
+        }
+        if ($arquivo === null) {
+            $arquivo = $diretorio . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log';
+        } else {
+            $arquivo = $diretorio . DIRECTORY_SEPARATOR . $arquivo;
+            $arquivoDiretorio = pathinfo($arquivo, PATHINFO_DIRNAME);
+            if (!is_dir($arquivoDiretorio)) {
+                mkdir($arquivoDiretorio, 0777, true);
+            }
+        }
+        $dataHora = date('Y-m-d H:i:s');
+        $mensagemFormatada = "[$dataHora] $mensagem" . PHP_EOL;
+        $handle = fopen($arquivo, 'a');
+        fwrite($handle, $mensagemFormatada);
+        fclose($handle);
+
+    }
+
+    public function viewLog($data) {
+        if (preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $data)) {
+            list($dia, $mes, $ano) = explode('/', $data);
+            $data = "$ano-$mes-$dia";
+        }
+        $diretorio = dirname(__FILE__) . DIRECTORY_SEPARATOR . "Logs";
+        $arquivo = $diretorio . DIRECTORY_SEPARATOR . $data . '.log';
+        if (file_exists($arquivo)) {
+            $linhas = file($arquivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+            return implode("<br>", $linhas);
+        }
+        return "O arquivo de log para a data especificada não foi encontrado.";
+    }
+
+    public function fullLog($diretorio = 'Logs', $formato = 'Y-m-d') {
+        $caminhoCompleto = dirname(__FILE__) . DIRECTORY_SEPARATOR . $diretorio . DIRECTORY_SEPARATOR;
+        $arquivos = glob($caminhoCompleto . '*.log');
+
+        // Função de comparação personalizada para ordenar os arquivos pela data no nome
+        usort($arquivos, function ($a, $b) {
+            $dataA = pathinfo($a, PATHINFO_FILENAME);
+            $dataB = pathinfo($b, PATHINFO_FILENAME);
+            return strtotime($dataA) - strtotime($dataB);
+        });
+        $conteudoCompleto = '';
+        foreach ($arquivos as $arquivo) {
+            $linhas = file($arquivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+            $conteudoArquivo = implode("\n", $linhas);
+            $conteudoCompleto .= '<pre>' . $conteudoArquivo . '</pre>' . PHP_EOL;
+        }
+        return $conteudoCompleto;
+    }
 }
--- src/MMHospedagem/FaleWhats/FaleWhats.php
+++ src/MMHospedagem/FaleWhats/FaleWhats.php
@@ -7,10 +7,13 @@ class FaleWhats {
     protected $id_sessao;
     protected $simular_presenca;
 
-    public function __construct($url, $licenca, $id_sessao, $simular_presenca) {
+    protected $log;
+
+    public function __construct($url, $licenca, $id_sessao, $simular_presenca, $log) {
         $this->url = $url;
         $this->licenca = $licenca;
         $this->id_sessao = $id_sessao;
         $this->simular_presenca = $simular_presenca;
+        $this->log = $log;
     }
 }
